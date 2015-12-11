7ba5a231613e11872c8bc084e9494c332ca55518
jtolos <Mrfalken3>
2015-12-11 13:45:27 +0000

    Initial import

---
+++ src/main/java/com/thomsonreuters/arch/eureka/us/EurekaUS.java
@@ -0,0 +1,15 @@
+package com.thomsonreuters.arch.eureka.us;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaUS {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaUS.class, args);
+	}
+
+}
